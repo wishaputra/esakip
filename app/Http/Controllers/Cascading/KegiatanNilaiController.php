60f3eb784b87f934a2fe0d924c677ccc2b67a4b5
skeduzz <wishasonic@gmail.com>
2024-08-09 08:40:03 +0700

    .

--- app/Http/Controllers/Cascading/KegiatanNilaiController.php
+++ app/Http/Controllers/Cascading/KegiatanNilaiController.php
@@ -3,61 +3,50 @@
 namespace App\Http\Controllers\Cascading;
 
 use App\Http\Controllers\Controller;
-use App\Models\Cascading\Model_Kegiatan_Nilai;
-use App\Models\Cascading\Model_program_Nilai;
-use Illuminate\Support\Facades\Auth;
-use App\Models\Cascading\Model_Visi;
-use App\Models\Cascading\Model_Misi;
 use App\Models\Cascading\Model_Kegiatan;
+use App\Models\Cascading\Model_visi;
 use App\Models\Cascading\Model_Kegiatan_Indikator;
+use App\Models\Cascading\Model_Kegiatan_Nilai;
+use App\Models\Cascading\Model_Program_Nilai;
 use Illuminate\Http\Request;
-use yajra\DataTables\DataTables;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use yajra\DataTables\DataTables;
 
 class KegiatanNilaiController extends Controller
 {
     public function api(Request $request)
     {
-        $kegiatan_nilai = Model_Kegiatan_Nilai::whereid_indikator_kegiatan($request->id_indikator_kegiatan)->get();
-    
+        $kegiatan_nilai = Model_Kegiatan_Nilai::where('id_indikator_kegiatan', $request->id_indikator_kegiatan)->get();
+
         return DataTables::of($kegiatan_nilai)
             ->addColumn('action', function ($p) {
                 return "
-                    <a  href='#' onclick='edit(" . $p->id . ")' title='Edit'><i class='icon-pencil mr-1'></i></a>
+                    <a href='#' onclick='edit(" . $p->id . ")' title='Edit'><i class='icon-pencil mr-1'></i></a>
                     <a href='#' onclick='remove(" . $p->id . ")' class='text-danger' title='Hapus'><i class='icon-remove'></i></a>";
             })
             ->rawColumns(['action'])
             ->toJson();
     }
-   
+
     public function index(Request $request)
     {
         $id_indikator_kegiatan = $request->id_indikator_kegiatan;
-        if (!$id_indikator_kegiatan || !Model_Kegiatan_Indikator::whereid($id_indikator_kegiatan)->first()) {
+        if (!$id_indikator_kegiatan || !Model_Kegiatan_Indikator::where('id', $id_indikator_kegiatan)->first()) {
             return redirect()->route('setup.kegiatan_indikator.index');
         }
 
-      
-        $indikator = Model_Kegiatan_Indikator::whereid($id_indikator_kegiatan)->get();
+        $indikator = Model_Kegiatan_Indikator::where('id', $id_indikator_kegiatan)->get();
         $tahun  = Model_Visi::all();
 
-
-        // return view('cascading.kegiatan_indikator.index', compact('title', 'id_visi', 'visi'));
-        return view('cascading.kegiatan_nilai.index', compact('indikator','id_indikator_kegiatan','tahun'));
+        return view('cascading.kegiatan_nilai.index', compact('indikator', 'id_indikator_kegiatan', 'tahun'));
     }
 
-   
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
 {
     $request->validate([
@@ -70,7 +59,7 @@ class KegiatanNilaiController extends Controller
         "capaian" => 'required',
     ]);
 
-    // Store the new Kegiatan Nilai
+    // Create the new entry in cascading_kegiatan_nilai
     $kegiatanNilai = Model_Kegiatan_Nilai::create([
         "id_indikator_kegiatan" => $request->id_indikator_kegiatan,
         "satuan" => $request->satuan,
@@ -82,59 +71,61 @@ class KegiatanNilaiController extends Controller
         "creator" => Auth::user()->id,
     ]);
 
-    // Update the pagu for the associated Kegiatan
-    $this->updatePaguKegiatan($request->id_indikator_kegiatan);
+    // Get the id_kegiatan related to the indikator_kegiatan
+    $id_kegiatan = DB::table('cascading_kegiatan_indikator')
+                    ->join('cascading_kegiatan', 'cascading_kegiatan.id', '=', 'cascading_kegiatan_indikator.id_kegiatan')
+                    ->where('cascading_kegiatan_indikator.id', $request->id_indikator_kegiatan)
+                    ->pluck('cascading_kegiatan.id')
+                    ->first();
 
-    // Calculate the total pagu for the program
-    $id_program = Model_Kegiatan_Indikator::find($request->id_indikator_kegiatan)
-        ->kegiatan
-        ->id_program;
+    // Calculate total pagu for the kegiatan
+    $totalPaguKegiatan = DB::table('cascading_kegiatan_nilai')
+                        ->join('cascading_kegiatan_indikator', 'cascading_kegiatan_nilai.id_indikator_kegiatan', '=', 'cascading_kegiatan_indikator.id')
+                        ->where('cascading_kegiatan_indikator.id_kegiatan', $id_kegiatan)
+                        ->sum('cascading_kegiatan_nilai.pagu');
 
-    $totalPaguProgram = Model_Kegiatan_Nilai::whereHas('kegiatan_indikator', function ($query) use ($id_program) {
-        $query->whereHas('kegiatan', function ($query) use ($id_program) {
-            $query->where('id_program', $id_program);
-        });
-    })->sum('pagu');
+    // Update the total pagu in cascading_kegiatan
+    $model_kegiatan = Model_Kegiatan::find($id_kegiatan);
+    if ($model_kegiatan) {
+        $model_kegiatan->update(["pagu" => $totalPaguKegiatan]);
+    }
 
-    $programNilai = Model_Program_Nilai::whereHas('program_indikator', function ($query) use ($id_program) {
-        $query->where('id_program', $id_program);
-    })->first();
+    // Calculate total pagu for the program
+    $id_program = DB::table('cascading_kegiatan')
+                    ->where('id', $id_kegiatan)
+                    ->pluck('id_program')
+                    ->first();
 
-    if ($programNilai) {
-        $programNilai->update(['pagu' => $totalPaguProgram]);
-    } else {
-        // Optionally, handle the case where no `Model_Program_Nilai` exists for this program
+    $totalPaguProgram = DB::table('cascading_kegiatan_nilai')
+                        ->join('cascading_kegiatan_indikator', 'cascading_kegiatan_nilai.id_indikator_kegiatan', '=', 'cascading_kegiatan_indikator.id')
+                        ->join('cascading_kegiatan', 'cascading_kegiatan_indikator.id_kegiatan', '=', 'cascading_kegiatan.id')
+                        ->where('cascading_kegiatan.id_program', $id_program)
+                        ->sum('cascading_kegiatan_nilai.pagu');
+
+    // Update the total pagu in cascading_program_nilai
+    $model_program_nilai = Model_Program_Nilai::where('id_indikator_program', $id_program)->first();
+    if ($model_program_nilai) {
+        $model_program_nilai->update(["pagu" => $totalPaguProgram]);
     }
 
     return response()->json(["message" => "Berhasil menambahkan data!"], 200);
 }
 
 
-
-
-   
     public function show($id)
     {
         //
     }
 
-   
     public function edit($id)
     {
-        return Model_Kegiatan_nilai::find($id);
+        return Model_Kegiatan_Nilai::find($id);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
 {
-    $kegiatanNilai = Model_Kegiatan_Nilai::find($id);
-    if (!$kegiatanNilai) {
+    $model_kegiatan_nilai = Model_Kegiatan_Nilai::find($id);
+    if (!$model_kegiatan_nilai) {
         return response()->json(["message" => "Data tidak ditemukan!"], 404);
     }
 
@@ -147,9 +138,8 @@ class KegiatanNilaiController extends Controller
         "capaian" => 'required',
     ]);
 
-    $oldPagu = $kegiatanNilai->pagu;
-
-    $kegiatanNilai->update([
+    // Update the kegiatan nilai data
+    $model_kegiatan_nilai->update([
         "satuan" => $request->satuan,
         "tahun" => $request->tahun,
         "triwulan" => $request->triwulan,
@@ -159,100 +149,95 @@ class KegiatanNilaiController extends Controller
         "creator" => Auth::user()->id,
     ]);
 
-    $this->updatePaguKegiatan($kegiatanNilai->id_indikator_kegiatan);
+    // Get the id_kegiatan related to the indikator_kegiatan
+    $id_kegiatan = DB::table('cascading_kegiatan_indikator')
+                    ->join('cascading_kegiatan', 'cascading_kegiatan.id', '=', 'cascading_kegiatan_indikator.id_kegiatan')
+                    ->where('cascading_kegiatan_indikator.id', $model_kegiatan_nilai->id_indikator_kegiatan)
+                    ->pluck('cascading_kegiatan.id')
+                    ->first();
 
-    $id_program = $kegiatanNilai->kegiatan_indikator->kegiatan->id_program;
+    // Update total pagu for kegiatan
+    $totalPaguKegiatan = DB::table('cascading_kegiatan_nilai')
+                        ->join('cascading_kegiatan_indikator', 'cascading_kegiatan_nilai.id_indikator_kegiatan', '=', 'cascading_kegiatan_indikator.id')
+                        ->where('cascading_kegiatan_indikator.id_kegiatan', $id_kegiatan)
+                        ->sum('cascading_kegiatan_nilai.pagu');
 
-    $totalPaguProgram = Model_Kegiatan_Nilai::whereHas('kegiatan_indikator', function ($query) use ($id_program) {
-        $query->whereHas('kegiatan', function ($query) use ($id_program) {
-            $query->where('id_program', $id_program);
-        });
-    })->sum('pagu');
+    $model_kegiatan = Model_Kegiatan::find($id_kegiatan);
+    if ($model_kegiatan) {
+        $model_kegiatan->update(["pagu" => $totalPaguKegiatan]);
+    }
 
-    $programNilai = Model_Program_Nilai::whereHas('program_indikator', function ($query) use ($id_program) {
-        $query->where('id_program', $id_program);
-    })->first();
+    // Calculate total pagu for the program
+    $id_program = DB::table('cascading_kegiatan')
+                    ->where('id', $id_kegiatan)
+                    ->pluck('id_program')
+                    ->first();
 
-    if ($programNilai) {
-        $programNilai->update(['pagu' => $totalPaguProgram - $oldPagu + $request->pagu]);
+    $totalPaguProgram = DB::table('cascading_kegiatan_nilai')
+                        ->join('cascading_kegiatan_indikator', 'cascading_kegiatan_nilai.id_indikator_kegiatan', '=', 'cascading_kegiatan_indikator.id')
+                        ->join('cascading_kegiatan', 'cascading_kegiatan_indikator.id_kegiatan', '=', 'cascading_kegiatan.id')
+                        ->where('cascading_kegiatan.id_program', $id_program)
+                        ->sum('cascading_kegiatan_nilai.pagu');
+
+    $model_program_nilai = Model_Program_Nilai::where('id_indikator_program', $id_program)->first();
+    if ($model_program_nilai) {
+        $model_program_nilai->update(["pagu" => $totalPaguProgram]);
     }
 
     return response()->json(["message" => "Berhasil merubah data!"], 200);
 }
 
 
-
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+public function destroy($id)
 {
+    // Find the Kegiatan Nilai by ID
     $kegiatanNilai = Model_Kegiatan_Nilai::find($id);
+
     if (!$kegiatanNilai) {
         return response()->json(["message" => "Data tidak ditemukan!"], 404);
     }
 
-    $id_indikator_kegiatan = $kegiatanNilai->id_indikator_kegiatan;
-    $oldPagu = $kegiatanNilai->pagu;
+    // Get the id_kegiatan related to the indikator_kegiatan
+    $id_kegiatan = DB::table('cascading_kegiatan_indikator')
+                    ->join('cascading_kegiatan', 'cascading_kegiatan.id', '=', 'cascading_kegiatan_indikator.id_kegiatan')
+                    ->where('cascading_kegiatan_indikator.id', $kegiatanNilai->id_indikator_kegiatan)
+                    ->pluck('cascading_kegiatan.id')
+                    ->first();
 
+    // Delete the Kegiatan Nilai entry
     $kegiatanNilai->delete();
 
-    $this->updatePaguKegiatan($id_indikator_kegiatan);
+    // Update total pagu for kegiatan
+    $totalPaguKegiatan = DB::table('cascading_kegiatan_nilai')
+                        ->join('cascading_kegiatan_indikator', 'cascading_kegiatan_nilai.id_indikator_kegiatan', '=', 'cascading_kegiatan_indikator.id')
+                        ->where('cascading_kegiatan_indikator.id_kegiatan', $id_kegiatan)
+                        ->sum('cascading_kegiatan_nilai.pagu');
 
-    $id_program = $kegiatanNilai->kegiatan_indikator->kegiatan->id_program;
-
-    $totalPaguProgram = Model_Kegiatan_Nilai::whereHas('kegiatan_indikator', function ($query) use ($id_program) {
-        $query->whereHas('kegiatan', function ($query) use ($id_program) {
-            $query->where('id_program', $id_program);
-        });
-    })->sum('pagu');
-
-    $programNilai = Model_Program_Nilai::whereHas('program_indikator', function ($query) use ($id_program) {
-        $query->where('id_program', $id_program);
-    })->first();
-
-    if ($programNilai) {
-        $programNilai->update(['pagu' => $totalPaguProgram - $oldPagu]);
+    $model_kegiatan = Model_Kegiatan::find($id_kegiatan);
+    if ($model_kegiatan) {
+        $model_kegiatan->update(["pagu" => $totalPaguKegiatan]);
     }
 
-    return response()->json(["message" => "Berhasil menghapus data!"], 200);
-}
+    // Calculate total pagu for the program
+    $id_program = DB::table('cascading_kegiatan')
+                    ->where('id', $id_kegiatan)
+                    ->pluck('id_program')
+                    ->first();
 
+    if ($id_program) {
+        $totalPaguProgram = DB::table('cascading_kegiatan_nilai')
+                            ->join('cascading_kegiatan_indikator', 'cascading_kegiatan_nilai.id_indikator_kegiatan', '=', 'cascading_kegiatan_indikator.id')
+                            ->join('cascading_kegiatan', 'cascading_kegiatan_indikator.id_kegiatan', '=', 'cascading_kegiatan.id')
+                            ->where('cascading_kegiatan.id_program', $id_program)
+                            ->sum('cascading_kegiatan_nilai.pagu');
 
-
-private function updatePaguKegiatan($id_indikator_kegiatan)
-    {
-        $kegiatanIndikator = Model_Kegiatan_Indikator::find($id_indikator_kegiatan);
-        if ($kegiatanIndikator) {
-            $id_kegiatan = $kegiatanIndikator->id_kegiatan;
-
-            // Sum pagu from all related kegiatan_nilai
-            $totalPagu = Model_Kegiatan_Nilai::where('id_indikator_kegiatan', $id_indikator_kegiatan)->sum('pagu');
-
-            // Find the related kegiatan_nilai and update its pagu
-            $kegiatanNilai = Model_Kegiatan_Nilai::where('id_indikator_kegiatan', $id_indikator_kegiatan)->first();
-            if ($kegiatanNilai) {
-                $kegiatanNilai->update(['pagu' => $totalPagu]);
-            }
-
-            // Also update the related program_nilai
-            $id_program = $kegiatanIndikator->kegiatan->id_program;
-            $totalPaguProgram = Model_Kegiatan_Nilai::whereHas('kegiatan_indikator', function ($query) use ($id_program) {
-                $query->whereHas('kegiatan', function ($query) use ($id_program) {
-                    $query->where('id_program', $id_program);
-                });
-            })->sum('pagu');
-
-            $programNilai = Model_Program_Nilai::where('id_indikator_program', $id_program)->first();
-            if ($programNilai) {
-                $programNilai->update(['pagu' => $totalPaguProgram]);
-            }
+        $model_program_nilai = Model_Program_Nilai::where('id_indikator_program', $id_program)->first();
+        if ($model_program_nilai) {
+            $model_program_nilai->update(["pagu" => $totalPaguProgram]);
         }
     }
 
+    return response()->json(["message" => "Data berhasil dihapus!"], 200);
+}
 
 }
